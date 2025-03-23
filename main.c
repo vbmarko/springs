dc04988754981946b5a4cd970dbd9e491f92a0ae
VileBile <markovinkl@gmail.com>
2025-03-23 12:08:26 +0100

    some drawing stuff

--- main.c
+++ main.c
@@ -10,17 +10,85 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "raylib.h"
 #include "gsl_funs.h"
 
 
 #define dt 0.1
 
+
+
 typedef struct point point ;
 typedef struct edge edge;
 typedef struct Cursor Cursor;
 typedef struct springs springs;
 typedef struct plane plane;
+typedef struct pix_homotopy pix_homotopy ;
+
+
+double dist(double x0 ,double y0, double x1, double y1){
+	return fabs(x0-x1) + fabs(y0-y1);
+
+}
+
+
+int dist_e(int x0 ,int y0, int x1, int y1){
+	int dx = x0 -x1, dy = y0 - y1;
+	return round(sqrt(dx*dx + dy*dy));
+}
+
+struct pix_homotopy{
+	int x00,y00, x01, y01;
+	int x10,y10, x11, y11;
+	Color c;
+};
+
+int max(int a, int b){
+	return a*(a>b) + b*(b>=a);
+}
+int min(int a, int b){
+	return b*(a>b) + a*(b>=a);
+}
+
+double lintrp(double a, double b, double t){
+	return (1-t)*a + t*b;
+}
+
+
+void upd_homotopy_pix(pix_homotopy *f, Color *pix,int w ,int h, double (*intrp)(double a, double b,double t)){
+	int d = max(dist(f->x00,f->y00,f->x10,f->y10),dist(f->x01,f->y01,f->x11,f->y11));
+	//printf("d = %d\n",d);
+	double x0,y0,x1,y1,t_d;
+	int x,y;
+	double t_s, t_t;
+	for(int s = 0;s<=d;++s){
+		t_s = 1.0*s/d;
+		x0 = intrp(f->x00,f->x10,t_s);
+		y0 = intrp(f->y00,f->y10,t_s);
+		x1 = intrp(f->x01,f->x11,t_s);
+		y1 = intrp(f->y01,f->y11,t_s);
+		t_d = dist(x0, y0, x1,y1);
+		
+		//printf("x0 = %d y0 = %d, x1 = %d, y1 = %d \n",x0,y0,x1,y1);
+		//printf("s = %d d = %d, t_d = %d \n",s,d,t_d);
+		for(int t = 0;t <= t_d;++t){
+			t_t = 1.0*t/t_d;
+			x = round(intrp(x0, x1, t_t));
+			y = round(intrp(y0, y1, t_t));
+			//printf("(x,y) = (%d,%d)\n------------------------\n",x,y);
+			if(x < 0 | x >= w | y < 0 | y >= h){
+					continue;
+				}
+			pix[w*y +x] = (f->c);
+
+		}
+	}
+		
+}
+
+
+
 
 struct Cursor{
 	gsl_vector *x_2d;
@@ -39,6 +107,8 @@ struct point{
 struct edge{
 	point *p_i, *p_j;
 	double k,l,beta;
+	int id, s, next_s;
+	Color c;
 };
 
 struct springs{
@@ -61,6 +131,18 @@ struct plane{
 };
 
 
+void free_plane(plane *Pi){
+	gsl_vector_free(Pi->r);
+	gsl_vector_free(Pi->P_x);
+	gsl_vector_free(Pi->P_y);
+	gsl_vector_free(Pi->n);
+	gsl_vector_free(Pi->rot_ax);
+	gsl_vector_free(Pi->scrtch);
+	gsl_matrix_free(Pi->rot_mat);
+
+}
+
+
 
 void init_point(point *p,int n,double m,double r){
 	gsl_vector *x = gsl_vector_calloc(n);
@@ -69,7 +151,11 @@ void init_point(point *p,int n,double m,double r){
 	}
 	gsl_vector *v = gsl_vector_calloc(n);
 	gsl_vector *normal = gsl_vector_calloc(n);
+	//gsl_vector_set(normal, 0, 1);
 	gsl_vector_set(normal, n-1, 1);
+	double nn = gsl_blas_dnrm2(normal);
+	if(nn){
+	gsl_vector_scale(normal, 1/(nn));};
 	double xn;
 	gsl_blas_ddot(x, normal, &xn);
 	gsl_vector_axpby(-xn,normal , 1, x);
@@ -82,6 +168,7 @@ void free_point(point *p){
 	gsl_vector_free(p->x);
 	gsl_vector_free(p->x_2d);
 	gsl_vector_free(p->v);
+	gsl_vector_free(p->n);
 }
 
 
@@ -113,6 +200,9 @@ springs *init_springs_from_adjacency(gsl_matrix *A,gsl_vector *m,gsl_vector *r,i
 				(e+e_id)->l = l;
 				(e+e_id)->k = 0.1;
 				(e+e_id)->beta = 1;
+				(e+e_id)->id = e_id;
+				(e+e_id)->s = 2;
+				(e+e_id)->c = BLACK;
 				e_id += 1;
 
 			}
@@ -123,9 +213,20 @@ springs *init_springs_from_adjacency(gsl_matrix *A,gsl_vector *m,gsl_vector *r,i
 	return s;
 }
 
+void free_springs(springs *s){
+	for(int i = 0;i<s->n_p;++i){
+		free_point(s->p + i);
+	}
+	free(s->p); 
+	free(s->e);
+}
+
 void electric(point *p1,point *p2,gsl_vector *scrtch){
 	gsl_vector_memcpy(scrtch,p1->x);
 	gsl_vector_axpby(-1, p2->x, 1, scrtch);
+	for(int i = 0;i<scrtch->size;++i){
+		scrtch->data[scrtch->stride*i] += 0.0000000000000000000000000000000000000000*(1.0*rand()/RAND_MAX - 0.5);
+	}
 	double d;
 	d = gsl_blas_dnrm2(scrtch);
 	double dqdt = dt*(p1->q)*(p2->q)/(d*d*d);
@@ -248,9 +349,11 @@ void upd_edge(edge *e,gsl_vector *scrtch){
 	gsl_vector *x_ij = scrtch;
 	gsl_vector_memcpy(x_ij,p_i->x);
 	gsl_vector_sub(x_ij,p_j->x);
-	gsl_vector_add_constant(x_ij,0.000000000000000000000000000000000001);
+	for(int i = 0;i<x_ij->size;++i){
+		x_ij->data[i*x_ij->stride] += 0.000000000000000000000000000000000000*(1.0*rand()/RAND_MAX - 0.5);
+	}
 	double d = gsl_blas_dnrm2(x_ij);
-	gsl_vector_scale(x_ij, 1/(d+0.00000000000001));
+	gsl_vector_scale(x_ij, 1/(d));
 	double diff_dt_k = e->k*dt*(e->l-d)/e->beta;
 	gsl_vector_axpby(-diff_dt_k/p_j->m,x_ij,1/e->beta,p_j->v);
 	gsl_vector_axpby(diff_dt_k/p_i->m,x_ij,1/e->beta,p_i->v);
@@ -285,14 +388,26 @@ void upd_edge_pix(edge *e,Color *pix,int w, int h){
 	int jx = e->p_j->int_x, jy = e->p_j->int_y;
 	int dx = ix-jx, dy = iy-jy;
 	double d = sqrt(dx*dx + dy*dy);
-	for(int i = 0;i<d;++i){
-		int x = round((i/(d-1))*jx + (d-1 -i)/(d-1)*ix);
-		int y = round((i/(d-1))*jy + (d-1 -i)/(d-1)*iy);
-		if(x < 0 | x >= w | y < 0 | y >= h){
-			continue;
-		}
-		pix[w*y +x] = BLACK;
+	if(((ix<0) &(jx<0)) | (((ix>= w) & (jx >= w))) |  ((iy<0) &(jy<0)) | (((iy>= h) & (jy >= h))) ){
+		//printf("we skippin \n");
+		return;
 	}
+	for(int i = 0;i<d;++i){
+		for(int r = -1;r<1 ;++r){
+			for(int s = -1;s<1;++s){
+				int x = round((i/(d-1))*jx + (d-1 -i)/(d-1)*ix) + r;
+				int y = round((i/(d-1))*jy + (d-1 -i)/(d-1)*iy) + s;
+				//printf("(x,y) =  (%d,%d)    ;",x,y);
+				if(x < 0 | x >= w | y < 0 | y >= h){
+					continue;
+				}
+				pix[w*y +x] = (e->c);
+
+
+			}
+		}
+	}
+	//printf("\n");
 
 
 
@@ -316,6 +431,10 @@ void update_springs_pix(springs *s,plane *Pi,Color *pix ,int w, int h,double sc)
 		(s->p+i)->int_x = int_x;
 		(s->p+i)->int_y = int_y;
 		int ro, co, idx;
+		if(int_x*int_x+int_y*int_y - int_r*int_r > w*w +h*h){
+			//printf("we skippin\n");
+			continue;
+		}
 		for(int k = -int_r/2;k<int_r/2;++k){
  			co = int_x+k;
 			if((co >=  w) | (co < 0)){
@@ -332,6 +451,7 @@ void update_springs_pix(springs *s,plane *Pi,Color *pix ,int w, int h,double sc)
 			    pix[idx] = BLUE;
 			}
 		}
+		printf("\n");
 	
 	}
 	for(int i = 0;i<s->n_e;++i){
@@ -339,6 +459,96 @@ void update_springs_pix(springs *s,plane *Pi,Color *pix ,int w, int h,double sc)
 		upd_edge_pix(e,pix,  w,  h);
 	}
 
+}
+
+
+int are_incident(edge *e1, edge *e2){
+	point *p1i = e1->p_i, *p1j = e1->p_j;
+	point *p2i = e1->p_i, *p2j = e1->p_j;
+	return 0 < ((p1i == p2i) + (p1i == p2j) + (p1j == p2i) + (p1j == p2j));
+}
+
+void upd_edge_s(edge* e,int n_e,double p12,double p20,double p_extern,double p_transmit){
+	double t = 1.0*rand()/RAND_MAX;
+	if(e->s == 1){
+		if(t< p12){
+			e->next_s = 2;
+			return;
+		}
+	}else if (e->s == 2) {
+		if(t < p20){
+			e->next_s = 0;
+			return;
+		}
+
+	
+	}else{
+		if(t < p_extern){
+			e->next_s = 1;
+			return;
+		}else {
+			edge *n = e - e->id;
+			for(int i = 0 ;i <n_e;++i){
+				if(i == e->id){
+					continue;
+				}
+				//printf("are incident = %d",are_incident(e,n+i));
+				if(are_incident(e, n+i) & (((n+i)->s) == 1)){
+					if (1.0*rand()/RAND_MAX < p_transmit){
+						printf("we transmitin\n");
+						e->next_s = 1;
+						return;
+					}
+
+				}
+
+			}
+		
+		}
+
+
+	}
+}
+
+void upd_edges_s(edge* e,int n_e,double p12,double p20,double p_extern,double p_transmit){
+	for(int i = 0;i<n_e;++i){
+		upd_edge_s(e+i, n_e, p12, p20,p_extern, p_transmit);
+	}
+	for(int i = 0;i<n_e;++i){
+		(e+i)->s = (e+i)->next_s;
+		int s = (e+i)->s;
+		if(s == 0){
+			(e+i)->c = BLUE;
+		}
+		if(s == 1){
+			(e+i)->c = RED;
+		}
+		if(s == 2){
+			(e+i)->c = BLACK;
+		}
+
+	}
+}
+
+void upd_edges_s_rand(edge* e,int n_e,double p12,double p20,double p_extern,double p_transmit){
+	int i = floor(n_e*1.0*rand()/RAND_MAX);
+	
+		upd_edge_s(e+i, n_e, p12, p20,p_extern, p_transmit);
+	
+
+		(e+i)->s = (e+i)->next_s;
+		int s = (e+i)->s;
+		if(s == 0){
+			(e+i)->c = BLUE;
+		}
+		if(s == 1){
+			(e+i)->c = RED;
+		}
+		if(s == 2){
+			(e+i)->c = BLACK;
+		}
+
+	
 }
 
 
@@ -396,45 +606,42 @@ int main(){
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(w, h, "");
-	SetTargetFPS(60);
 	Image img = GenImageColor(w, h, BLACK);
-	// set the image's format so it is guaranteed to be aligned with our pixel buffer format below
-	//ImageFormat(&img, PIwELFORMAh_UNCOMPRESSED_R8G8B8A8);
 	Texture tex = LoadTextureFromImage(img);
 
 	Color rgba_pixels[h * w]; // 4 channels
 
 
-	// make sure to set ALL ALPHA CHANNELS of the pixels (every fourth index) to 255, otherwise you'll have transparent pixels
 	int sw = GetScreenWidth(), sh = GetScreenHeight();	
-	// push the rgba pixel values to the texture
 	Rectangle rec = (Rectangle) {.x = 0,.y = 0,.width = w,.height=h};
 	Rectangle big_rec =(Rectangle) {.x= 0,.y=0,.width = sw,.height = sh};
 	int a = 0;
 	int scale = 1;
 	int b = 1;
 	double x0 =0, y0 = 0,sc= 1;
-	//return 0;
+	double p12 = 1, p20 = 0.5, p_transmit = 0.8;
+	double ha = 0.0000000000000000000000000001;
+	double hq = 1000000000000000;
+	double p_extern = 1 - exp(-ha*dt);
+	p12 = 1 - exp(-hq*dt);
+
+
+	pix_homotopy f = {.x00=w/2+50,.y00=h/2,.x01=w/2+50,.y01=h/2,.x10=w/2 -100,.y10=h/2 + 100,.x11=w/2 + 100,.y11= h/2,.c=BLACK};
+
 	while(!WindowShouldClose()){
+		p_extern = 1 - exp(-ha*dt);
+		p12 = 1 - exp(-hq*dt);
 		sw = GetScreenWidth(); sh = GetScreenHeight();	
 		upd_cursor(&cursor, &Pi,sc,sw,sh);
 		big_rec.width = sw; big_rec.height = sh;
-		if(b){
-		upd_springs(s);}
+		if(b | IsKeyPressed(KEY_UP)){
+		upd_springs(s);
+		upd_edges_s_rand(s->e, s->n_e,p12,p20,p_extern,p_transmit);
+				}
 		for(int i = 0;i<h*w;++i){
 			rgba_pixels[i] = WHITE;
 		}
-		for(int i = 0;i<s->n_p;++i){
-			point *p = s->p +i;
-			for(int j = 0;j<p->x->size;++j){
-				printf("%g,",gsl_vector_get(p->x, j));
-			}
-			printf("    ");
-			for(int j = 0;j<p->x->size;++j){
-				printf("%g,",gsl_vector_get(p->n, j));
-			}
-			printf("\n");
-		}
+
 		update_springs_pix(s,&Pi, rgba_pixels, w, h,sc);
 			
 
@@ -464,7 +671,24 @@ int main(){
 
 			gsl_vector_axpby(dt, Pi.P_x, 1, Pi.r);
 		}
-	
+		if(IsKeyDown(KEY_T)){
+			ha = ha*(1+dt);
+
+		}
+		if(IsKeyDown(KEY_G)){
+			ha = ha*(1-dt);
+		}		
+		if(IsKeyDown(KEY_Z)){
+			hq = hq*(1+dt);
+
+		}
+		if(IsKeyDown(KEY_H)){
+			hq = hq*(1-dt);
+		}
+
+
+		upd_homotopy_pix(&f,rgba_pixels,w,h,lintrp);
+		
 	
 		UpdateTexture(tex, rgba_pixels);
 		//GenTextureMipmaps(&tex);
@@ -475,6 +699,9 @@ int main(){
 		//reset window and draw texture
 		ClearBackground(RAYWHITE);
 		DrawTexturePro(tex,rec,big_rec,(Vector2){0,0},0,WHITE);
+		DrawFPS(0,0);
+		DrawText(TextFormat("p_e = %g",p_extern), 0, 20,20, BLACK);
+		DrawText(TextFormat("p_12 = %g",p12), 0, 40,20, BLACK);
 		for(int i = 0;i<s->n_e;++i){
 			//printf("%d\n",i);
 			edge *e = s->e + i;
@@ -488,6 +715,9 @@ int main(){
 		float dsc = GetMouseWheelMove();
 		sc = (1+0.01*dsc)*sc;
 		}
+	free_springs(s);
+	free_plane(&Pi);
+	free(s);
 	UnloadTexture(tex);
 	CloseWindow();
 	return 0;}
